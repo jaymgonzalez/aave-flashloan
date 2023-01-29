f3308d02f3d7bfe57eb4b37abdfc6a8ca0c9bdc9
Jay M Gonzalez <jaimungon@gmail.com>
2023-01-29 19:06:03 +0100

    add contract contructor + deploy

---
+++ contracts/FlashLoan.sol
@@ -0,0 +1,18 @@
+// contracts/FlashLoan.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
+
+contract FlashLoan is FlashLoanSimpleReceiverBase {
+    address payable owner;
+
+    constructor(
+        address _addressProvider
+    ) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {
+        owner = payable(msg.sender);
+    }
+
+    
