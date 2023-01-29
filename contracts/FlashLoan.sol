22e526de6c3e759827534af32a3be9fbb7f4f734
Jay M Gonzalez <jaimungon@gmail.com>
2023-01-29 19:50:04 +0100

    add helper functs

--- contracts/FlashLoan.sol
+++ contracts/FlashLoan.sol
@@ -9,6 +9,14 @@ import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contract
 contract FlashLoan is FlashLoanSimpleReceiverBase {
     address payable owner;
 
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Only the contract owner can call this function"
+        );
+        _;
+    }
+
     constructor(
         address _addressProvider
     ) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {
@@ -57,4 +65,15 @@ contract FlashLoan is FlashLoanSimpleReceiverBase {
             referralCode
         );
     }
+
+    function getBalance(address _tokenAddress) external view returns (uint256) {
+        return IERC20(_tokenAddress).balanceOf(address(this));
+    }
+
+    function withdraw(address _tokenAddress) external onlyOwner {
+        IERC20 token = IERC20(_tokenAddress);
+        token.transfer(msg.sender, token.balanceOf(address(this)));
+    }
+
+    receive() external payable {}
 }
